1023c4616e4cb00e59de97da327d75bd67fd1fcb
Bengalize <121535934+Bengalize@users.noreply.github.com>
2024-07-14 07:12:11 -0400

    Add script for basic password management

---
+++ basic-python-scripts/src/password_manager.py
@@ -0,0 +1,35 @@
+print("Bonjour")
+
+
+def save_password():
+    Username = input("Please give the username: ")
+    Password = input("Please give the password associated: ")
+
+    with open("password.txt", "w") as f:
+        f.write(Username + " | " + Password + "\n")
+
+
+def load_password():
+    with open("password.txt", "r") as f:
+        for line in f.readline():
+            data = line.rstrip()
+            username, password = data.split("|")
+            print("Username: " + username + "| Password: " + password)
+
+
+while True:
+    command = input("What would You like to do?(save/load/quit?) ").lower()
+
+    if command == "save":
+        save_password()
+    elif command == "load":
+        load_password()
+    elif command == "quit":
+        break
+    else:
+        print("Incorrect, Please enter a correct command ")
+        continue
+
+print("Have a good day!")
+
+quit()
